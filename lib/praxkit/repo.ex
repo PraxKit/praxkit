50552cbb9d80ee0d943fb3bbbcec5086b8bdfcb3
ZooSky <zoosky@gmail.com>
2018-11-18 22:03:26 +0100

    upgrade phx 1.4

--- lib/praxkit/repo.ex
+++ lib/praxkit/repo.ex
@@ -1,11 +1,5 @@
 defmodule Praxkit.Repo do
-  use Ecto.Repo, otp_app: :praxkit
-
-  @doc """
-  Dynamically loads the repository url from the
-  DATABASE_URL environment variable.
-  """
-  def init(_, opts) do
-    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
-  end
+  use Ecto.Repo,
+    otp_app: :praxkit,
+    adapter: Ecto.Adapters.Postgres
 end
--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -5,7 +5,7 @@ defmodule PraxkitWeb.ConnCase do
 
   Such tests rely on `Phoenix.ConnTest` and also
   import other functionality to make it easier
-  to build common datastructures and query the data layer.
+  to build common data structures and query the data layer.
 
   Finally, if the test case interacts with the database,
   it cannot be async. For this reason, every test runs
@@ -19,20 +19,20 @@ defmodule PraxkitWeb.ConnCase do
     quote do
       # Import conveniences for testing with connections
       use Phoenix.ConnTest
-      import PraxkitWeb.Router.Helpers
+      alias PraxkitWeb.Router.Helpers, as: Routes
 
       # The default endpoint for testing
       @endpoint PraxkitWeb.Endpoint
     end
   end
 
-
   setup tags do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Praxkit.Repo)
+
     unless tags[:async] do
       Ecto.Adapters.SQL.Sandbox.mode(Praxkit.Repo, {:shared, self()})
     end
+
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
-
 end
