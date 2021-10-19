485d99c4672be8896a32386122269d8b25b422cb
Zoo Sky <zoosky@gmail.com>
2021-10-19 19:09:05 +0200

    deployment

--- praxkit/config/runtime.exs
+++ praxkit/config/runtime.exs
@@ -18,6 +18,8 @@ if config_env() == :prod do
     # ssl: true,
     # socket_options: [:inet6],
     url: database_url,
+    # IMPORTANT: Or it won't find the DB server
+    socket_options: [:inet6],
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
@@ -32,14 +34,23 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
+  app_name =
+        System.get_env("FLY_APP_NAME") ||
+          raise "FLY_APP_NAME not available"
+
   config :praxkit, PraxkitWeb.Endpoint,
+    server: true,
+    url: [host: "#{app_name}.fly.dev", port: 80],
+    cache_static_manifest: "priv/static/cache_manifest.json",
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: String.to_integer(System.get_env("PORT") || "4000")
+      port: String.to_integer(System.get_env("PORT") || "4000"),
+      # IMPORTANT: support IPv6 addresses
+      transport_options: [socket_opts: [:inet6]]
     ],
     secret_key_base: secret_key_base
 
