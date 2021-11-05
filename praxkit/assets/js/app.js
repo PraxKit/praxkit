76e2e79ff92a4640319a47275b1548b697905517
Zoo Sky <zoosky@gmail.com>
2021-11-05 15:32:32 +0100

    add alpine, tailwind libs, rearange topbar

--- praxkit/assets/js/app.js
+++ praxkit/assets/js/app.js
@@ -24,7 +24,13 @@ import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
-import topbar from "../vendor/topbar"
+import topbar from "topbar"
+// import Alpine
+import Alpine from "alpinejs"
+
+// Add this before your liveSocket call.
+window.Alpine = Alpine;
+Alpine.start();
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
