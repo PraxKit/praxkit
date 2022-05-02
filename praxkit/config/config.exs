dbce429f7b50454537d04f43003bb6166d32bced
Zoo Sky <zoosky@gmail.com>
2022-05-02 21:44:46 +0200

    tailwind configs, working?

--- praxkit/config/config.exs
+++ praxkit/config/config.exs
@@ -42,7 +42,8 @@ config :praxkit, PraxkitWeb.Endpoint,
 config :esbuild,
   version: "0.12.26",
   default: [
-    args: ~w(js/app.js vendor/fonts/InterWeb/inter.css --bundle --loader:.woff2=file --loader:.woff=file --target=es2016 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+    # args: ~w(js/app.js vendor/fonts/InterWeb/inter.css --bundle --loader:.woff2=file --loader:.woff=file --target=es2016 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+    args: ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets),
 
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
